bbb6bd276fca31180671689790c28bbec0a4a4e5
GabrielDantasDs <gabri4k@gmail>
2025-10-24 16:30:48 -0300

    frontend static output

---
+++ frontend/out/_next/static/chunks/879dc2d48ad83bcd.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/4f1ff8e0f4907560.js",
+  "static/chunks/b5695eed8a88f51e.js",
+  "static/chunks/turbopack-121d2d0e9d39efef.js"
+])
---
+++ frontend/out/_next/static/chunks/ab6efc9d8ea0535a.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/7f329f9b77be0c9e.js",
+  "static/chunks/b5695eed8a88f51e.js",
+  "static/chunks/turbopack-80cf629520b2246a.js"
+])
